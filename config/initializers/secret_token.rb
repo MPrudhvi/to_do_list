48925b1f4263a88fe99f4eaffd0af0270ebf48e3
Prudhvi Maguluru <i.m.prudhvi@gmail.com>
2012-01-10 01:35:30 -0700

    A rails application named to_do_list is created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToDoList::Application.config.secret_token = 'a87d5d6a9361169610fb0643a681f3d19f3351d20c451cbd14b12703232fec88a4d3f748f19bd888c015176e3ccccdd7adbde18f29f7863bff8300aee9c2da5a'
